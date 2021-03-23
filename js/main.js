01b6cd54b674de37cf0008ba1f014e7373300ff2
Arm Sukrit <arm.xdsk@hotmail.com>
2021-03-23 11:10:12 +0700

    not working yet

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
-function addAge() {
-  let today = new Date();
-  const bd = new Date(2000, 6, 29);
-  let diffInDays = Math.floor(
-    (today.getTime() - bd.getTime()) / (1000 * 3600 * 24)
-  );
-  let years = Math.floor(diffInDays / 365);
-
-  document.getElementById("age").innerText = years;
-  console.log("added age");
+function addEmailToButton() {
+    const email = document.getElementById("email-text").innerText;
+    document.getElementById("email-me-btn").href = "mailto:" + email;
+    console.log("added email");
 }
 
-function addEmail() {
-  const email = document.getElementById("email-text").innerText;
-  document.getElementById("email-me-btn").href = "mailto:" + email;
-  console.log("added email");
-}
+const addContent = () => {
+    const content = fetch("./content/content.json");
+    content.then(() => {
+        console.log(content);
+
+        // add avatar
+        document.querySelector(".avatar").src = content.avatar;
+        // add name
+        document.querySelector("#first-name").innerText = content.name.first;
+        document.querySelector("#last-name").innerText = content.name.last;
+        // add job
+        document.querySelector(".job").innerText = content.job;
+        // add about
+        document.querySelector(".about").innerText = content.about;
+        // add email
+        document.querySelector("#email-text").innerText = content.email;
+        addEmailToButton();
+        // add facebook info
+        document.querySelector("#facebook").innerText = content.facebook.name;
+        document.querySelector("#facebook").href = content.facebook.link;
+        // add location info
+        document.querySelector("#location").innerText = content.location.name;
+        document.querySelector("#location").href = content.location.link;
+    });
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  addAge();
-  addEmail();
+    addContent();
 });
