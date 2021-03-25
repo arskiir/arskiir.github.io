0d0beb9868e98c60c6b3d830cbe6adf618f70acd
Arm Sukrit <arm.xdsk@hotmail.com>
2021-03-25 15:55:58 +0700

    still not working but soon...

--- js/main.js
+++ js/main.js
@@ -5,29 +5,33 @@ function addEmailToButton() {
 }
 
 const addContent = () => {
-    const content = fetch("./content/content.json");
-    content.then(() => {
-        console.log(content);
+    const content = fetchContent("../content/content.json");
+    console.log(content);
+    // add avatar
+    document.querySelector(".avatar").src = content.avatar;
+    // add name
+    document.querySelector("#first-name").innerText = content.name.first;
+    document.querySelector("#last-name").innerText = content.name.last;
+    // add job
+    document.querySelector(".job").innerText = content.job;
+    // add about
+    document.querySelector(".about").innerText = content.about;
+    // add email
+    document.querySelector("#email-text").innerText = content.email;
+    addEmailToButton();
+    // add facebook info
+    document.querySelector("#facebook").innerText = content.facebook.name;
+    document.querySelector("#facebook").href = content.facebook.link;
+    // add location info
+    document.querySelector("#location").innerText = content.location.name;
+    document.querySelector("#location").href = content.location.link;
+};
 
-        // add avatar
-        document.querySelector(".avatar").src = content.avatar;
-        // add name
-        document.querySelector("#first-name").innerText = content.name.first;
-        document.querySelector("#last-name").innerText = content.name.last;
-        // add job
-        document.querySelector(".job").innerText = content.job;
-        // add about
-        document.querySelector(".about").innerText = content.about;
-        // add email
-        document.querySelector("#email-text").innerText = content.email;
-        addEmailToButton();
-        // add facebook info
-        document.querySelector("#facebook").innerText = content.facebook.name;
-        document.querySelector("#facebook").href = content.facebook.link;
-        // add location info
-        document.querySelector("#location").innerText = content.location.name;
-        document.querySelector("#location").href = content.location.link;
-    });
+const fetchContent = async (url) => {
+    // fetch json
+    const response = await fetch(url);
+    console.log(response);
+    return response;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
